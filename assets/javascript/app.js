c2827272ed1aef85c597b611f571670d92392459
Eshaffer <edmond.shaffer@gmail.com>
2018-07-14 17:36:45 -0400

    2nd

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,14 +1,18 @@
-$(document).ready();
 
-var answer = [];
-var correctAnswers =[];
-var numCorrect = 0;
-var numWrong = 0;
-var numUnAnswered = 0;
-var gameStart = false;
+
+var newArray = [];
+var holder =[];
+var numberCorrect = 0;
+var numberWrong = 0;
+var numberUnAnswered = 0;
 var answerPick = "";
-var timer = ;
-var counter = ;
+var timeRunning = false ;
+var counter = 30;
+var intervalId;
+var index;
+var pick;
+// var questionCount = questionChoice.length;
+
 
 
 var questionChoice = [
@@ -34,20 +38,83 @@ var questionChoice = [
     },
     
     {
-        question:"What was Mario origanlly called?"
-        choices: ["Mario","Mr.Plumber" "Luigi","Jump Man"],
-        answer: 3;
+        question:"What was Mario origanlly called?",
+        choices: ["Mario","Mr.Plumber","Luigi","Jump Man"],
+        answer: 3,
 
     },
     
 
 ];
 
-
+$("#reStart").hide();
 $("#start").on("click",function () {
     $("#start").hide();
     displayQuestion();
-    runTimer();
-    for(var i = o; i < questionChoice.length; i++)
+    startTimer();
+    for(var i = 0; i < questionChoice.length; i++){
+        holder.push(questionChoice[i]);
+    }
     
 })
+
+function startTimer(){
+    if (!timeRunning) {
+        intervalId = setInterval(decrement,1000)
+        timeRunning = true;
+    }
+}
+
+function decrement(){
+    $("#timeRemaining").html("<h3>Time Left" + counter + "</h3>");
+    counter --;
+    numberUnAnswered ++;
+    if(counter === 0){
+        stop()
+        ("#answerBlock").html("<p> Time's up! The correct answer is: " + pick.choce[pick.answer] + "</p>")
+        hidepicture();
+    }
+
+}
+
+function stop(){
+    timeRunning = false;
+    clearInterval(intervalId);
+}
+
+function displayQuestion(){
+    index = Math.floor(Math.random() * questionChoice.length);
+    pick = questionChoice[index];
+
+    for(var i = 0; i < pick.choices.lenght; i++){
+        var playerChoice = $("<div>");
+        playerChoice.addClass("answerchoice");
+        playerChoice.html(pick.choice[i]);
+        playerChoice.attr("data-guessvalue", i);
+        $("#answerBlock").append(playerChoice);
+
+    }
+}
+
+$('.answerchoice').on("clikc", function(){
+    answerPick = parseInt($(this).attr("data-guessvalue"));
+    if(answerPick === pick.answer){
+        stop();
+        numberCorrect++;
+        answerPick="";
+        $("#answerBlock").html("<p>Correct!</p>");
+        hidepicture();
+    }
+    else{
+        stop();
+        numberWrong++;
+        answerPick=""
+        ("#answerBlock").html("<p> Wrong! The correct answer is: " + pick.choce[pick.answer] + "</p>")
+        hidepicture();
+    
+    }
+})
+
+
+
+   
